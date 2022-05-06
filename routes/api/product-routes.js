2f367735c04ff588d645671703365e8f5ce6a08e
Fausto Lopez <lopezfausto123@yahoo.com>
2022-05-06 02:17:57 -0700

    errors with put/post

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -61,15 +61,8 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+router.post('/', (req, res) => {
+
   Product.create(req.body)({
     product_name: req.body.product_name,
     price: req.body.price,
